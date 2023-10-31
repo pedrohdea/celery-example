9f9bc74a4e446feed4bb90476817fe6d0207beba
Pedroa Assumpção <pedro.assumpcao@beemon.com.br>
2023-10-31 14:37:52 -0300

    Adicionado tutorial comentado

--- contrib/worker.sh
+++ contrib/worker.sh
@@ -1,3 +1,6 @@
 # https://docs.celeryq.dev/en/stable/userguide/workers.html#starting-the-worker
+# Com virtualenv
+
+# celery -A celery_app worker -l info --pool=solo -Q hello,matematica,mail
 
 celery -A celery_app worker -l info -c 4 -Q hello,matematica,mail
--- example/tasks.py
+++ example/tasks.py
@@ -5,10 +5,13 @@ from celery import current_app, Task
 from celery.contrib import rdb
 from loguru import logger
 
+@current_app.task(bind=True)
+def task_debug(self: Task, mail):
+    rdb.set_trace
 
 @current_app.task(queue='hello')
 def ola_mundo():
-    time.sleep(3)
+    time.sleep(30)
     logger.success('log: olá mundo')
     return 'olá mundo'
 
@@ -56,6 +59,10 @@ class MyTask(Task):
         logger.critical('on failure')
         return super().on_failure(exc, task_id, args, kwargs, einfo)
 
+    def on_success(self, retval, task_id, args, kwargs):
+        logger.success('on failure')
+        return super().on_success(retval, task_id, args, kwargs)
+
 
 @current_app.task(queue='mail', base=MyTask)
 def task_enviar_email(email: str):
@@ -65,9 +72,9 @@ def task_enviar_email(email: str):
     raise NotImplementedError('Implemente envio de e-mail')
 
 
+
+
 # https://docs.celeryq.dev/en/main/userguide/debugging.html
-
-
 @current_app.task(bind=True, queue='mail')
 def task_testar_email(self, email):
     logger.info(f'e-mail recebido {email}')
